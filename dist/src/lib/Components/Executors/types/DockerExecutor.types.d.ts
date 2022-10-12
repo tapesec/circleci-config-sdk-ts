75c774f6fe8826007663ef8509a8cfe2f845b1f4
Lionnel DUPOUY <lionnel.dupouy@algolia.com>
2022-10-12 15:15:26 +0200

    chore: commit dist folder
    To temporary use this forked package until my PR be accepted

---
+++ dist/src/lib/Components/Executors/types/DockerExecutor.types.d.ts
@@ -0,0 +1,17 @@
+import { StringParameter } from '../../Parameters/types';
+import { DockerImageShape } from '../exports/DockerImage';
+import { AnyResourceClassBase } from './Executor.types';
+/**
+ * A JSON representation of the Docker Executor Schema
+ * To be converted to YAML
+ */
+export declare type DockerExecutorContentsShape = DockerImageShape[];
+export declare type DockerImageMap = {
+    image: StringParameter;
+};
+/**
+ * The available Docker Resource Classes.
+ * @see {@link https://circleci.com/docs/2.0/configuration-reference/#docker-executor} for specifications of each class.
+ */
+export declare type DockerResourceClass = AnyResourceClassBase;
+//# sourceMappingURL=DockerExecutor.types.d.ts.map
